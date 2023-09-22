b995c25df7e739f59213f9029702d2d681b93339
ashraf <Zeeshan@LAPTOP-TG9PRPCJ>
2023-09-22 18:42:06 -0400

    this is my first commit.

---
+++ MavenProj_ForSeAuto/src/test/java/webAutomationExamples/HelloWorld.java
@@ -0,0 +1,33 @@
+package webAutomationExamples;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, World !");
+		
+		//System.setProperty("webdriver.chrome.driver","C:\\Users\\Zeeshan\\browserDrivers\\chrome\\chromedriver.exe");
+		
+		// WebDriverManager.chromedriver().setup();
+		 ///WebDriverManager.edgedriver().setup();
+		
+		/*
+		 * WebDriverManager.chromedriver().setup();
+           WebDriverManager.firefoxdriver().setup();
+           WebDriverManager.edgedriver().setup();
+           WebDriverManager.operadriver().setup();
+           WebDriverManager.chromiumdriver().setup()
+           WebDriverManager.iedriver().setup();
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+		
+	
+		             //driver.quit();
+		               
+	}
+
+}
---
+++ MavenProj_ForSeAuto/src/test/java/webAutomationExamples/LoginTest.java
@@ -0,0 +1,14 @@
+package webAutomationExamples;
+
+public class LoginTest {
+
+	public static final String browser ="Edge";
+	
+	public static void main(String[] args) {
+		
+		System.out.println("browser name ="+ browser);
+		//driver.quit();   //close all opened browsers
+		
+	}
+
+}
