7cbdc35d44151a85abef699aa4c08d0aeeaea19f
Zeeshan <bynmoh@yahoo.com>
2023-10-16 12:05:13 -0400

    commit for adding new file to github

---
+++ MavenProj_ForSeAuto/src/test/java/webAutomationExamples/DemoTestForAddingGitHub.java
@@ -0,0 +1,11 @@
+package webAutomationExamples;
+
+public class DemoTestForAddingGitHub {
+
+	public static void main(String[] args) {
+		
+		      System.out.println("This is a verifying file for pushing from eclipse--staging ---local git repo to GitHub remote repo.");
+
+	}
+
+}
