279eb09c0724be26babf84691af2dfc0a77f8226
Zeeshan <bynmoh@yahoo.com>
2023-10-16 12:12:53 -0400

    2nd commit for github

--- MavenProj_ForSeAuto/src/test/java/webAutomationExamples/DemoTestForAddingGitHub.java
+++ MavenProj_ForSeAuto/src/test/java/webAutomationExamples/DemoTestForAddingGitHub.java
@@ -5,7 +5,7 @@ public class DemoTestForAddingGitHub {
 	public static void main(String[] args) {
 		
 		      System.out.println("This is a verifying file for pushing from eclipse--staging ---local git repo to GitHub remote repo.");
-
+		      System.out.println("This is a verifying file for *** second pushing *****  from eclipse--staging ---local git repo to GitHub remote repo.");
 	}
 
 }
