1d23aeccf1749c0396bf5b086c06fc009f35f790
Zeeshan <bynmoh@yahoo.com>
2023-10-16 13:44:10 -0400

    First Commit in Develop Branch

---
+++ MavenProj_ForSeAuto/src/test/java/webAutomationExamples/ConditionIfElse.java
@@ -0,0 +1,16 @@
+package webAutomationExamples;
+
+public class ConditionIfElse {
+
+	public static void main(String[] args) {
+		
+		int time = 20;
+		if (time < 18) {
+		  System.out.println("Good day.");
+		} else {
+		  System.out.println("Good evening.");
+		}
+		// Outputs "Good evening."
+	}
+
+}
